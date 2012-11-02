e00f5a4efd040a0aaf003fefde8595721f544fa2
Samy Dindane <samy@dindane.com>
2012-11-02 17:21:46 +0100

    [ZSH] Add the "grepi" and "rgrepi"

--- dinduks.zsh
+++ dinduks.zsh
@@ -1,10 +1,12 @@
 source ~/.rvm/scripts/rvm
 
 alias grep="grep --color=auto"
+alias grepi="grep -i --color=auto"
 alias less="less -r"
 alias l="ls -alhG"
 alias ll="ls -lhG"
 alias rgrep="grep -r --color=auto"
+alias rgrepi="grep -ri --color=auto"
 alias rs="bundle exec rails server"
 alias rspec="rspec --color"
 alias sshad="ssh dinduks@ssh.alwaysdata.com"
