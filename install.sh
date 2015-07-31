cd82da41227dd0576028b1b92722e921041a9eca
Samy Dindane <samy@dindane.com>
2015-07-31 03:20:40 +0200

    Add the alias "lock" for i3lockscreen in install.sh

--- install.sh
+++ install.sh
@@ -1,6 +1,7 @@
 sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
 
 sudo ln -s $PWD/scripts/i3lockscreen /usr/local/bin
+sudo ln -s /usr/local/bin/{i3lockscreen,lock}
 
 sudo ln -s $PWD/systemd/i3lock.service /etc/systemd/system
 sudo systemctl enable i3lock.service
