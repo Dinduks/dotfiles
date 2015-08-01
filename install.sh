6a667b073a2bcc8af0a8703d653b5d066429e3c6
Samy Dindane <samy@dindane.com>
2015-08-01 03:22:32 +0200

    Make install.sh install all the ACPI events in acpi/

--- install.sh
+++ install.sh
@@ -17,11 +17,9 @@ sudo systemctl daemon-reload
 
 sudo mkdir -p /etc/acpi
 sudo mkdir -p /etc/acpi/events
-sudo ln -s $PWD/acpi/fkey-lock    /etc/acpi/events
-sudo ln -s $PWD/acpi/fkey-lock.sh /etc/acpi
-sudo ln -s $PWD/acpi/volkey-mute  /etc/acpi/events
-sudo ln -s $PWD/acpi/volkey-up    /etc/acpi/events
-sudo ln -s $PWD/acpi/volkey-down  /etc/acpi/events
+for event in acpi/*; do
+  sudo ln -s $PWD/$event /etc/acpi/events
+done
 
 ln -s $PWD/vim/.vim ~
 ln -s $PWD/vim/.vimrc ~
