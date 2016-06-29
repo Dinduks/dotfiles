0b639a0ddac612e273ffb3b7b5c9492c321752c0
Samy Dindane <samy@dindane.com>
2016-06-30 01:16:48 +0200

    Revert "Update screenlayouts/"
    This reverts commit a5af425cdcf343c29b8d66b0d0fccca18f25a325.

--- screenlayouts/1366x768.sh
+++ screenlayouts/1366x768.sh
@@ -3,4 +3,4 @@
 xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --off --output LVDS1 --mode 1366x768 --pos 0x0 --rotate normal --output VGA1 --off
 
 wallpaper=$(ls ~/Pictures/Wallpapers/16:9/ | shuf -n 1)
-feh --bg-fill "$HOME/Pictures/Wallpapers/16:9/$wallpaper"
+feh --bg-fill ~/Pictures/Wallpapers/16:9/$wallpaper
--- screenlayouts/2560x1080.sh
+++ screenlayouts/2560x1080.sh
@@ -3,4 +3,4 @@
 xrandr --output VIRTUAL1 --off --output DP3 --off --output DP2 --off --output DP1 --off --output HDMI3 --off --output HDMI2 --off --output HDMI1 --mode 2560x1080_50.00 --pos 0x0 --rotate normal --output LVDS1 --off --output VGA1 --off
 
 wallpaper=$(ls ~/Pictures/Wallpapers/21:9/ | shuf -n 1)
-feh --bg-fill "$HOME/Pictures/Wallpapers/21:9/$wallpaper"
+feh --bg-fill ~/Pictures/Wallpapers/21:9/$wallpaper
