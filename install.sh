9fdbd18d3a9d8d1693c1f04e86a93261b76991ea
Samy Dindane <samy@dindane.com>
2016-07-13 01:16:23 +0200

    Update install.sh: Bye Debian, hello Arch

--- install.sh
+++ install.sh
@@ -1,64 +1,137 @@
-sudo aptitude -y install i3 i3lock xautolock vim zsh scrot imagemagick htop \
-                         curl wget acpid pulseaudio unclutter terminator awk \
-                         rxvt-unicode-256color feh ttf-bitstream-vera \
-                         network-manager-pptp-gnome compton rofi apt apt-file \
-                         manpages-dev manpages-posix-dev i3blocks pasystray \
-                         pavucontrol fonts-font-awesome network-manager-gnome \
-                         pv shutter redshift
+function install_git {
+  sudo pacman -S --needed git
+}
 
-sudo curl https://raw.githubusercontent.com/Dinduks/change-execute-loop/master/cel -o /usr/local/bin/cel
+function install_yaourt {
+  cd /tmp
+  git clone https://aur.archlinux.org/package-query.git
+  cd package-query
+  makepkg -si
+  cd ..
+  git clone https://aur.archlinux.org/yaourt.git
+  cd yaourt
+  makepkg -si
 
-sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+  yaourt -Syu --devel --aur
+}
 
-for job in cron.d/*; do
-  sudo cp $PWD/$job /etc/cron.d
-done
+function install_user_programs {
+  yaourt -S --needed i3-gaps-git i3lock zsh xautolock scrot imagemagick \
+    htop curl wget unclutter awk sysstat feh openssh gedit xclip compton rofi \
+    pavucontrol shutter arandr acpi acpid redshift-git gedit libreoffice \
+    thefuck tree ttf-dejavu ttf-arabeyes-fonts slack-desktop alsa-utils dunst \
+    cmus-git libmad flac cmusfm xorg-xmodmap xorg-server xorg-xinit openssh \
+    the_silver_searcher spotify xorg-xev redshift-gtk-git nautilus pv \
+    rxvt-unicode google-chrome thunderbird i3blocks weechat \
+    telegram-desktop-bin xorg-xrdb pkgfile pasystray pulseaudio \
+    linux-manpages ttf-font-awesome network-manager-applet xorg-xwininfo \
+    dwb-git jdk8-openjdk
+}
 
-for script in scripts/*; do
-  sudo ln -s $PWD/$script /usr/local/bin
-done
+function install_cel {
+  sudo curl https://raw.githubusercontent.com/Dinduks/change-execute-loop/master/cel -o /usr/local/bin/cel
+  sudo chmod +x /usr/local/bin/cel
+}
 
-sudo ln -s /usr/local/bin/{i3lockscreen,lock}
+function install_ohmyzsh {
+  sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+}
 
-sudo cp $PWD/systemd/i3lock.service /etc/systemd/system
-sudo systemctl enable i3lock.service
-sudo systemctl daemon-reload
+function install_hr {
+  sudo curl https://raw.githubusercontent.com/LuRsT/hr/master/hr -o /usr/local/bin/hr
+  sudo chmod +x /usr/local/bin/hr
+}
 
-sudo mkdir -p /etc/acpi
-sudo mkdir -p /etc/acpi/events
-for event in acpi/*; do
-  sudo ln -s $PWD/$event /etc/acpi/events
-done
+function setup_cron_jobs {
+  for job in cron.d/*; do
+    sudo cp $PWD/$job /etc/cron.d
+  done
+}
 
-ln -s $PWD/vim/.vim ~
-ln -s $PWD/vim/.vimrc ~
+function setup_scripts {
+  for script in scripts/*; do
+    sudo ln -s $PWD/$script /usr/local/bin
+  done
+}
 
-ln -s $PWD/git/.gitconfig ~
-ln -s $PWD/git/.gitignore_global ~
+function setup_i3lock {
+  sudo ln -s /usr/local/bin/{i3lockscreen,lock}
+  sudo cp $PWD/systemd/i3lock.service /etc/systemd/system
+  sudo systemctl enable i3lock.service
+  sudo systemctl daemon-reload
+}
 
-ln -s /home/dinduks/dotfiles/.Xdefaults ~
-xrdb -merge ~/.Xdefaults
+function setup_vim {
+  ln -s $PWD/vim/.vim ~
+  ln -s $PWD/vim/.vimrc ~
+}
 
-mkdir -p ~/.i3
-ln -s $PWD/i3/config ~/.i3
-ln -s $PWD/i3/.i3blocks.conf ~
+function setup_git {
+  ln -s $PWD/git/.gitconfig ~
+  ln -s $PWD/git/.gitignore_global ~
+}
 
-ln -s $PWD/dinduks.zsh ~/.oh-my-zsh/custom
+function create_screenshots_dir {
+  mkdir -p ~/Pictures/Screenshots
+}
 
-ln -s $PWD/urxvt ~/.urxvt
+function setup_xdefaults {
+  ln -s /home/dinduks/dotfiles/.Xdefaults ~
+  xrdb ~/.Xdefaults
+}
 
-mkdir -p ~/Pictures/Screenshots
+function setup_i3_config {
+  mkdir -p ~/.i3
+  ln -s $PWD/i3/config ~/.i3
+  ln -s $PWD/i3/.i3blocks.conf ~
+}
 
-ln -s $PWD/i3/*.png ~/Pictures
-ln -s $PWD/i3/*.jpg ~/Pictures
+function setup_zsh_config {
+  ln -s $PWD/dinduks.zsh ~/.oh-my-zsh/custom
+}
 
-mkdir -p ~/.local/share/applications
-rm ~/.local/share/applications/mimeapps.list
-ln -s $PWD/mimeapps.list ~/.local/share/applications
+function setup_urxvt_config {
+  rm ~/.urxvt
+  ln -s $PWD/urxvt ~/.urxvt
+}
 
-ln -s $PWD/compton.conf ~/.compton.conf
+function setup_mimeapps {
+  mkdir -p ~/.local/share/applications
+  mv ~/.local/share/applications/mimeapps.list{,--} 2> /dev/null
+  ln -s $PWD/mimeapps.list ~/.local/share/applications
+}
 
-mkdir -p $HOME/.config/dunst
-ln -s $PWD/dunstrc ~/.config/dunst/dunstrc
+function setup_compton_config {
+  ln -s $PWD/compton.conf ~/.compton.conf
+}
 
-sudo usermod -a -G pulse dinduks
+function setup_dunst_config {
+  mkdir -p $HOME/.config/dunst
+  ln -s $PWD/config/dunst/dunstrc ~/.config/dunst/dunstrc
+}
+
+function run {
+  echo -e "\e[33mRunning $1...\e[39m"
+  eval $1
+  echo -e "\e[33mFinished running $1.\e[39m\n"
+}
+
+run "install_git"
+run "install_yaourt"
+run "install_user_programs"
+run "install_cel"
+run "install_ohmyzsh"
+run "install_hr"
+run "setup_cron_jobs"
+run "setup_scripts"
+run "setup_vim"
+run "setup_git"
+run "create_screenshots_dir"
+run "setup_xdefaults"
+run "setup_dunst_config"
+run "setup_mimeapps"
+run "setup_compton_config"
+run "setup_i3_config"
+run "setup_zsh_config"
+run "setup_urxvt_config"
+run "setup_i3lock"
