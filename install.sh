2ef891071f00ad7ee1af5a58c5b82016bae212a6
Samy Dindane <samy@dindane.com>
2018-11-29 19:47:29 +0100

    install.sh: replace the i3-gaps-git package by i3-gaps

--- install.sh
+++ install.sh
@@ -28,7 +28,7 @@ function install_yaourt {
 }
 
 function install_user_programs {
-  yaourt -S --needed i3-gaps-git zsh xautolock scrot imagemagick \
+  yaourt -S --needed i3-gaps zsh xautolock scrot imagemagick \
     htop curl wget unclutter awk sysstat feh openssh gedit xclip compton rofi \
     pavucontrol shutter arandr acpi acpid gedit libreoffice thefuck tree \
     ttf-dejavu ttf-arabeyes-fonts slack-desktop alsa-utils dunst-git cmus-git \
